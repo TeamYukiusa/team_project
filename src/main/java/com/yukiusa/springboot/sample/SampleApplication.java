be75da44747714367c9adc5fb2ef4c1dada1c02f
yukiusa <yukiusa@arc-connects.com>
2017-12-17 21:07:29 +0900

    指摘修正

--- src/main/java/com/yukiusa/springboot/sample/SampleApplication.java
+++ src/main/java/com/yukiusa/springboot/sample/SampleApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SampleApplication {
 
+	/**
+	 * サンプル・アプリケーションメイン
+	 * @param args 引数
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
 	}
